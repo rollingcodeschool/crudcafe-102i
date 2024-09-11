d7c903441dea297069d38544790629a69824d5d1
Emilse Arias <emilsearias@gmail.com>
2024-09-11 09:43:18 -0300

    enviamos a la pagina principal desde el link de la pagina de error 404

--- src/components/pages/Erro404.jsx
+++ src/components/pages/Erro404.jsx
@@ -1,11 +1,13 @@
 import { Button } from "react-bootstrap";
 import error from "../../assets/error404.png";
+import { Link } from "react-router-dom";
+
 const Error404 = () => {
   return (
     <section className="mainSection text-center">
       <img src={error} alt="error 404" />
       <div>
-        <Button variant="primary">Volver al inicio</Button>
+        <Link className="btn btn-success" to='/'>Volver al inicio</Link>
       </div>
     </section>
   );
