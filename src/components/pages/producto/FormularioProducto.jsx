e9e62f80714b88ab78d0578e2a08aab5548183ff
Emilse Arias <emilsearias@gmail.com>
2024-09-11 11:20:26 -0300

    agregamos una variable de entorno para guardar la direccion de json-server

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,45 +1,102 @@
 import { Form, Button } from "react-bootstrap";
-import Swal from "sweetalert2";
+import { useForm } from "react-hook-form";
+import { crearProductoAPI } from "../../../helpers/queries";
 
 const FormularioProducto = () => {
- 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-  
+  const onSubmit = (producto) => {
+    console.log(producto);
+    //pedir a la api crear el producto
+    crearProductoAPI();
+  };
+
   return (
     <section className="container mainSection">
       <h1 className="display-4 mt-5">Nuevo producto</h1>
       <hr />
-      <Form className="my-4" >
+      <Form className="my-4" onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Producto*</Form.Label>
-          <Form.Control type="text" placeholder="Ej: Cafe"  />
-          <Form.Text className="text-danger"></Form.Text>
+          <Form.Control
+            type="text"
+            placeholder="Ej: Cafe"
+            {...register("nombreProducto", {
+              required: "El nombre del producto es un dato obligatorio",
+              minLength: {
+                value: 2,
+                message: "Debe ingresar como minimo 2 caracteres",
+              },
+              maxLength: {
+                value: 50,
+                message: "Debe ingresar como maximo 50 caracteres inclusive",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.nombreProducto?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
-          <Form.Control type="number" placeholder="Ej: 50"
-             />
-          <Form.Text className="text-danger"></Form.Text>
+          <Form.Control
+            type="number"
+            placeholder="Ej: 50"
+            {...register("precio", {
+              required: "El precio es un valor obligatorio",
+              min: {
+                value: 50,
+                message: "El precio minimo debe ser de $50 en adelante",
+              },
+              max: {
+                value: "20000",
+                message: "El precio maximo debe ser $20000",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.precio?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
-           
+            {...register("imagen", {
+              required: "La url de la imagen debe ser obligatoria",
+              pattern: {
+                value: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/,
+                message:
+                  "Debe ingresar una url de imagen valida, los formatos admitidos son (jpg|jpeg|gif|png) ",
+              },
+            })}
           />
-          <Form.Text className="text-danger"></Form.Text>
+          <Form.Text className="text-danger">
+            {errors.imagen?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoría*</Form.Label>
-          <Form.Select >
+          <Form.Select
+            {...register("categoria", {
+              required: "Debe seleccionar una categoria",
+            })}
+          >
             <option value="">Seleccione una opcion</option>
             <option value="Infusiones">Infusiones</option>
             <option value="Batidos">Batidos</option>
-            <option value="dulce">Dulce</option>
-            <option value="salado">Salado</option>
+            <option value="Dulce">Dulce</option>
+            <option value="Salado">Salado</option>
           </Form.Select>
-          <Form.Text className="text-danger"></Form.Text>
+          <Form.Text className="text-danger">
+            {errors.categoria?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción breve*</Form.Label>
@@ -47,9 +104,23 @@ const FormularioProducto = () => {
             type="text"
             placeholder="Ej: Una taza de café suave y aromático."
             as="textarea"
-           
+            {...register("descripcion_breve", {
+              required: "La descripcion breve es ogligatoria",
+              minLength: {
+                value: 5,
+                message:
+                  "Debe ingresar como minimo una descripcion de 5 caracteres",
+              },
+              maxLength: {
+                value: 50,
+                message:
+                  "Debe ingresar como maximo una descripcion de 50 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger"></Form.Text>
+          <Form.Text className="text-danger">
+            {errors.descripcion_breve?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción Amplia*</Form.Label>
@@ -57,9 +128,23 @@ const FormularioProducto = () => {
             type="text"
             placeholder="Ej: El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día."
             as="textarea"
-           
+            {...register("descripcion_amplia", {
+              required: "La descripcion amplia es ogligatoria",
+              minLength: {
+                value: 10,
+                message:
+                  "Debe ingresar como minimo una descripcion de 10 caracteres",
+              },
+              maxLength: {
+                value: 250,
+                message:
+                  "Debe ingresar como maximo una descripcion de 250 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger"></Form.Text>
+          <Form.Text className="text-danger">
+            {errors.descripcion_amplia?.message}
+          </Form.Text>
         </Form.Group>
 
         <Button type="submit" variant="success">
---
+++ src/helpers/queries.js
@@ -0,0 +1,12 @@
+const URLProducto = import.meta.env.VITE_API_PRODUCTO;
+
+//Peticiones o solicitudes
+
+//GET
+//PUT o PATH
+//DELETE
+
+//POST
+export const crearProductoAPI = ()=>{
+    console.log(URLProducto)
+}
