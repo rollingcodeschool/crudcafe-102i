d9d2e18530986f19f7b96219e7ba905207dfa53e
Emilse Arias <emilsearias@gmail.com>
2024-09-23 09:56:54 -0300

    agregamos las rutas del administrador en forma protegida

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Footer from "./components/common/Footer";
 import Menu from "./components/common/Menu";
-import Administrador from "./components/pages/Administrador";
 import Inicio from "./components/pages/Inicio";
+import Error404 from "./components/pages/Erro404";
+import DetalleProducto from "./components/pages/DetalleProducto";
+import Login from "./components/pages/Login";
+import RutaProtegida from "./components/routes/RutaProtegida";
+import RutasAdmin from "./components/routes/RutasAdmin";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import FormularioProducto from "./components/pages/producto/FormularioProducto";
-import Error404 from "./components/pages/Erro404";
-import DetalleProducto from "./components/pages/DetalleProducto";
-import Login from "./components/pages/Login";
-import { useState } from "react";
 
 function App() {
   const usuario = JSON.parse(sessionStorage.getItem('userKey')) || '';
@@ -23,10 +23,12 @@ function App() {
         <Routes> 
           <Route exact path="/" element={<Inicio></Inicio>}></Route>
           <Route exact path="/detalleproducto" element={<DetalleProducto></DetalleProducto>}></Route>
-          <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
-          <Route exact path="/administrador/crear" element={<FormularioProducto titulo='Nuevo producto' creandoProducto={true}></FormularioProducto>}></Route>
-          <Route exact path="/administrador/editar/:id" element={<FormularioProducto titulo='Editar producto' creandoProducto={false}></FormularioProducto>}></Route>
           <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}></Route>
+          <Route exact path="/administrador/*" element={
+            <RutaProtegida>
+              <RutasAdmin/>
+            </RutaProtegida>
+          }></Route>
           <Route path="*" element={<Error404></Error404>}></Route>
         </Routes> 
         <Footer></Footer>
--- src/components/routes/RutaProtegida.jsx
+++ src/components/routes/RutaProtegida.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { Navigate } from "react-router-dom";
 
-const RutaProtegida = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const RutaProtegida = ({ children }) => {
+  //agregar la logica que debe cumplir quien puede ver los children
+  const userAdmin = JSON.parse(sessionStorage.getItem("userKey")) || null;
+  //preguntar si no estoy logueado
+  if (!userAdmin) {
+    //no soy el administrador
+    return <Navigate to={"/login"}></Navigate>;
+  } else {
+    //si soy el administrador
+    return children;
+  }
 };
 
 export default RutaProtegida;
